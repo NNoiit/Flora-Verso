70f1565d56c035e6fe50e54fabd6f97666f0401b
Cristion A.Assis <57966693+NNoiit@users.noreply.github.com>
2022-03-31 17:01:28 -0300

    Flora

---
+++ site-flora-1/js/script.js
@@ -0,0 +1,25 @@
+function dados() {
+  window.alert("Flora");
+}
+
+function alteracorAlmbUm(){
+  let div = document.getElementsByClassName("text-center");
+  div[0].style.backgroundColor = 'rgb(255, 75, 136)';
+  
+  div = document.getElementsByClassName("navbar");
+  div[0].style.backgroundColor = 'rgb(255, 75, 136)';
+}
+
+function alteracorAlmbDois(){
+  let div = document.getElementsByClassName("text-center");
+    div[0].style.backgroundColor = 'rgb(17, 71, 77)';
+    div = document.getElementsByClassName("navbar");
+    div[0].style.backgroundColor = 'rgb(17, 71, 77)';
+}
+
+function alteracorAlmbTres(){
+  let div = document.getElementsByClassName("text-center");
+  div[0].style.backgroundColor = 'rgb(79, 157, 230)';
+  let bar = document.getElementsByClassName("navbar");
+  bar[0].style.backgroundColor = 'rgb(79, 157, 230)';
+}
